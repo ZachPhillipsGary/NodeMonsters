c90eb650747d7f35b44a73a54db94f5bc47b9dfd
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-17 23:41:18 -0400

    attempted to match

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ function authenticate(username,password) {
       //Iterate through rows to find match (safer way than dynamically creating query string)
       for (var i = 0; i < rows.length; i++) {
         console.log(username,rows[i].email);
-       if ( username == rows[i].email ) {
+       if ( username == String(rows[i].email) ) {
         // if (bcrypt.compareSync(password, rows[i].password)) {
               //username is in database and password matches
                 authenticated =  true;
