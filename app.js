dafddef6af9645a7cf83183fa6c6b7899fcb7886
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-20 02:34:44 -0400

    test

--- app.js
+++ app.js
@@ -104,6 +104,7 @@ function movePlayer(player, direction) {
                         users[player].x++;
                     break;
             }
+            console.log(player);
         }
     }
 }
