eb4d4c933dc948c7e9532da9c5a6860017cbb012
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-05-04 01:12:07 -0400

    added player label and tweaks

--- public/js/client.js
+++ public/js/client.js
@@ -40,6 +40,9 @@ function client(appIDs, chatField, username) {
                    console.log(String(mapState["onlineUsers"][player].direction))
                    var xCord = mapState[ "onlineUsers"][player].x * 25;
                    var yCord = mapState[ "onlineUsers"][player].y * 25;
+                   ctx.fillStyle = mapState[ "onlineUsers"][player].color;
+                   context.font = "bold 3px Arial";
+                    context.fillText(player, xCord, yCord);
                    var image = document.getElementById(String(mapState["onlineUsers"][player].direction));
                    ctx.drawImage(image, xCord, yCord, 25, 25);
 
